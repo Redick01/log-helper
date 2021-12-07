4549c389dafc4d57a9fbba09bef2441604d01af3
Redick01 <11090829@qq.com>
2021-12-07 21:14:32 +0800

    modify fegin

--- src/main/java/com/ruubypay/log/filter/web/WebConfiguration.java
+++ src/main/java/com/ruubypay/log/filter/web/WebConfiguration.java
@@ -1,17 +1,30 @@
 package com.ruubypay.log.filter.web;
 
+import cn.hutool.core.util.ObjectUtil;
+import cn.hutool.core.util.ReflectUtil;
+import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.*;
+
+import java.lang.reflect.Method;
 
 /**
  * @author liupenghui
  * @date 2021/6/30 11:41 上午
  */
-public class WebConfiguration extends WebMvcConfigurationSupport {
+public class WebConfiguration implements WebMvcConfigurer {
 
     @Override
-    protected void addInterceptors(InterceptorRegistry registry) {
-        // 日志链路追踪拦截器
-        registry.addInterceptor(new WebHandlerMethodInterceptor()).addPathPatterns("/**");
-        super.addInterceptors(registry);
+    public void addInterceptors(InterceptorRegistry registry) {
+        InterceptorRegistration interceptorRegistration;
+        interceptorRegistration = registry.addInterceptor(new WebHandlerMethodInterceptor());
+        //这里是为了兼容springboot 1.5.X，1.5.x没有order这个方法
+        try{
+            Method method = ReflectUtil.getMethod(InterceptorRegistration.class, "order", Integer.class);
+            if (ObjectUtil.isNotNull(method)){
+                method.invoke(interceptorRegistration, 1);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
